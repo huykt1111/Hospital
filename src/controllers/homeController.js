e5c5e168e5c1912fc49497342620326913c6d28c
huykt1111 <huylmht10@gmail.com>
2023-02-04 15:48:53 +0700

    CRUD USER DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from '../models/index';
+import CRUDSevice from '../services/CRUDSevice';
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -14,7 +16,19 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDSevice.createNewUser(req.body);
+    console.log(message);
+    return res.send('Post crud for server');
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 };
---
+++ src/services/CRUDSevice.js
@@ -0,0 +1,45 @@
+import db from '../models/index';
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId
+            })
+
+            resolve('OK! Create a new user success!');
+        } catch (e) {
+            reject(e);
+        }
+    })
+    console.log('data from service');
+    console.log(data);
+    console.log(hashPasswordFromBcrypt);
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+module.exports = {
+    createNewUser: createNewUser,
+}
