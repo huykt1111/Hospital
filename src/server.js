960c0377d5bdfd02706bf57f5d0633816d8b7982
huykt1111 <huylmht10@gmail.com>
2023-02-05 20:09:17 +0700

    Write APIS Select All Users

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
 import connectDB from "./config/connectDB";
+import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
+const corsOptions = {
+    origin: process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
