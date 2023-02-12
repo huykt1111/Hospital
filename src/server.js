82e225573481546039fcdada6940ce1ef39fed0e
huykt1111 <huylmht10@gmail.com>
2023-02-12 20:53:23 +0700

    Fix Image Type Long BLOB

--- src/server.js
+++ src/server.js
@@ -14,8 +14,13 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// config app
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
 
 viewEngine(app);
 initWebRouters(app);
--- src/services/userService.js
+++ src/services/userService.js
@@ -119,7 +119,8 @@ let createNewUser = (data) => {
                     phonenumber: data.phonenumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 });
                 resolve({
                     errCode: 0,
@@ -182,6 +183,9 @@ let updateUserData = (data) => {
                 user.gender = data.gender;
                 user.positionId = data.positionId;
                 user.roleId = data.roleId;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save();
 
